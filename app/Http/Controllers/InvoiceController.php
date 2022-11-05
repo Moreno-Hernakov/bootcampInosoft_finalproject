caf3d6a32fc46e3ea5ce2009262e87ade4ea5b76
farrel07 <farrqeel@gmail.com>
2022-11-05 21:35:43 +0700

    add edit invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -37,4 +37,21 @@ class InvoiceController extends Controller
         $data = $this->invoiceService->find($id['_id']);
         return response()->json($data,200);
     }
+
+    // untuk edit vendor invoice
+    public function edit(Request $request){
+        $validator = Validator::make($request->all(), [
+            'attachment'=>'mimes:pdf,doc,docx',
+            'document'=>'mimes:pdf,doc,docx',
+            'invoice_no' => 'required',
+            'id' => 'required'          
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+    
+        $credentials = $request->all();
+        return response()->json($this->invoiceService->editInvoice($credentials));
+    }
 }
