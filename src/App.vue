11ace3fa23739d32a00ff0a9e3ae96a2060e1f2c
muhBilal <mb336450@gmail.com>
2022-11-14 14:28:45 +0700

    home

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <Sidebar />
+
+    <section class="home">
+      <Navbar/>
+
+      <router-view/>
+    </section>
+      
+  
   </div>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue';
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Navbar
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div
+        class="pl-10 w-full h-16 bg-slate-100 shadow-sm flex navbar text-gray-500 dark:bg-[#242526] dark:text-white flex justify-end px-10">
+        <div class="flex items-center gap-3 font-medium">
+            <h1>Good Morning, Bilal</h1>
+            <img src="../assets/01.jpg" class="w-12 h-12 rounded-full">
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
