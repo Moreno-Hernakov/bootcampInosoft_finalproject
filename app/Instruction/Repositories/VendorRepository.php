67147686a33c4cd35afae4170774acad9379185f
Hafidzrdwn <hafidz10032005@gmail.com>
2022-12-20 17:01:49 +0700

    fix controller

--- app/Instruction/Repositories/VendorRepository.php
+++ app/Instruction/Repositories/VendorRepository.php
@@ -6,46 +6,46 @@ use App\Models\Vendor;
 
 class VendorRepository
 {
-    
+
 	/**
 	 * Untuk menambahkan vendor
 	 *  */
-    public function create(array $data)
-	{	
-		if($id = $this->getWhere('assigned_vendor',$data['assigned_vendor'])->first()) {
+	public function create(array $data)
+	{
+		if ($id = $this->getWhere('assigned_vendor', $data['assigned_vendor'])->first()) {
 
 			$vendor_address = isset($id['vendor_address']) ? $id['vendor_address'] : [];
 
 			$vendor_address[] = [
-				'_id'=> (string) new \MongoDB\BSON\ObjectId(),
-				'vendor_address'=>$data['vendor_address'],
+				'_id' => (string) new \MongoDB\BSON\ObjectId(),
+				'vendor_address' => $data['vendor_address'],
 			];
 
 			$id['vendor_address'] = $vendor_address;
 			$id['updated_at'] = time();
-    		$id->save();
+			$id->save();
 
 			return $id;
 		}
 
 		$vendor_address[] = [
-			'_id'=> (string) new \MongoDB\BSON\ObjectId(),
-			'vendor_address'=>$data['vendor_address'],
+			'_id' => (string) new \MongoDB\BSON\ObjectId(),
+			'vendor_address' => $data['vendor_address'],
 		];
 
 		$dataSaved = [
-			'assigned_vendor'=>$data['assigned_vendor'],
-			'vendor_address'=>$vendor_address,
-			'created_at'=>time()
+			'assigned_vendor' => $data['assigned_vendor'],
+			'vendor_address' => $vendor_address,
+			'created_at' => time()
 		];
 
-		$id = Vendor::create($dataSaved);	
-        $id->save();
-		
+		$id = Vendor::create($dataSaved);
+		$id->save();
+
 		return $id;
 	}
 
-    /**
+	/**
 	 * Untuk mendapatkan data vendor berdasarkan id
 	 *  */
 	public function find(string $id)
@@ -56,11 +56,12 @@ class VendorRepository
 
 	public function getAllAssignedVendor()
 	{
-		$vendor = Vendor::all('assigned_vendor');
+		$vendor = Vendor::all();
 		return $vendor;
 	}
 
-    public function getWhere($coloumn, $id){
+	public function getWhere($coloumn, $id)
+	{
 		return Vendor::where($coloumn, $id)->get();
 	}
 }
